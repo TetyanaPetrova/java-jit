266a31ac09d09765d63c6c9600de79617a603d41
TetyanaPetrova <t.petrova@enamine.net>
2024-02-08 22:01:51 +0200

    added 1234 test case

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,7 +4,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         System.out.println("Hello Git!");
-        System.out.println("Hello GitHub!");
+        System.out.println("Hello 1234_testCase!");
 
     }
 }
